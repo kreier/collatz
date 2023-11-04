4f6c00add85f39a0585ecea078dc0a1765b5f693
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-11-04 08:56:42 +0700

    output control, more details

--- python/collatz.py
+++ python/collatz.py
@@ -1,20 +1,38 @@
 # to be finished
 
-last_number = 100
+last_number = 10000
+
+max_max = 1
+max_steps = 1
+max_at = 1
+
+print_path = False
+print_numbers = False
 
 for i in range(1, last_number):
     x = i
-    steps = 1
+    steps = 0   # aka stopping time
     max = x
-    print(x, end='')
+    if print_numbers:
+        print(x, end='')
     while x != 1: 
         if x % 2 == 0:   # x is even
-            print(".", end='')
             x /= 2
+            if print_path:
+                print(".", end ='')
         else:
-            print("*", end='')
             x = 3 * x + 1
+            if print_path:
+                print("*", end='')
         if max < x:
-            max = x
+            max = int(x)
         steps += 1
-    print(f", {int(max)}, {steps}")
+    if print_path:
+        print(f", {max}, {steps}")
+    if max_max < max:
+        max_max = max
+        max_at = i
+    if max_steps < steps:
+        max_steps = steps
+print(f"For the investigated {last_number} numbers we reached a maximum height of {max_max} at {max_at}")
+print(f"We also found a maximum of {max_steps} steps.")
